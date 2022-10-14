0391b94c0d19c21ba9becdc406f4c413b09c0060
sarathchandra-27 <110110387+sarathchandra-27@users.noreply.github.com>
2022-10-14 22:22:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';//
+import './App.css';
+//import ComponentC from './Components/ComponentC'
+//import { UserProvider } from './Components/UserContext';
+//import Parent1 from './Components/Parent1';
+//import Tabular from './Components/Tabular';
+import Home from './Components/Sarath/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+export default App;
